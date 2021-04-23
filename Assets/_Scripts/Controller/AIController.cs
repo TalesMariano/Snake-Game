b5561f264d06c9bddf57bb2d8698bdaf583cd51c
TalesMariano <tales.mariano1@gmail.com>
2021-04-23 19:10:50 -0300

    Snake AI and Player Movement
    Add Prefab for Player and AI Snakes
    
    Add Controllers, for player and AI
    
    Add Vector2Functions, that add new function for Vector2int
    
    Add A* Pathfinder, with modification for this project

---
+++ Assets/_Scripts/Controller/AIController.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIController : MonoBehaviour, IController
+{
+    public Snake tempSnake;
+
+
+    [SerializeField] public Transform targetBlock;
+    public Vector2Int targetPos;
+
+    [SerializeField] private Vector2Int[] path;
+
+
+    private void OnEnable()
+    {
+        tempSnake.OnMove += PathAndUpdMove;
+    }
+
+    private void OnDisable()
+    {
+        tempSnake.OnMove -= PathAndUpdMove;
+    }
+
+    public void SetTarget(Block block, Vector2Int pos)
+    {
+        targetBlock = block.transform;
+        targetPos = pos;
+
+        //PathAndUpdMove();
+    }
+
+
+
+    //private AStarSearch starSearch = new AStarSearch();
+
+    void PathAndUpdMove()
+    {
+        GetPath();
+        UpdateDirection();
+
+    }
+
+    [ContextMenu("GetPath")]
+    void GetPath()
+    {
+        MapGrid grid = MapGrid.instance;
+
+        if (!targetBlock)
+            return;
+
+
+        targetPos = Vector2Int.FloorToInt(targetBlock.transform.position);
+
+
+        path = AStarSearch.StarSearch(grid, tempSnake.pos, targetPos);
+    }
+
+    void UpdateDirection()
+    {
+        if (path == null || path.Length == 0)
+        {
+            AvoidColision();
+        }
+        else
+        {
+            tempSnake.direction =  path[0] - tempSnake.pos; 
+        }
+    }
+
+
+    void AvoidColision()
+    {
+        MapGrid grid = MapGrid.instance;
+
+        if (!grid.PositionPassable(tempSnake.pos+tempSnake.direction))
+        {
+            if(grid.CheckPosValid(tempSnake.pos + Vector2Functions.TurnRight(tempSnake.direction)))
+                tempSnake.Turn(1);
+            else if (grid.CheckPosValid(tempSnake.pos + Vector2Functions.TurnLeft(tempSnake.direction)))
+                tempSnake.Turn(-1);
+        }
+    }
+
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+
+        if (path == null || path.Length < 2)
+            return;
+
+        for (int i = 0; i < path.Length - 1; i++)
+        {
+            Gizmos.DrawLine((Vector3Int)path[i], (Vector3Int)path[i + 1]);
+        }
+    }
+}
---
+++ Assets/_Scripts/Vector2Functions.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SnakeGame
+{
+    /*
+    */
+}
+
+public class Vector2Functions
+{
+    public static Vector2Int TurnRight(Vector2Int dir)
+    {
+        if (dir == Vector2Int.up)
+            return Vector2Int.right;
+        else if (dir == Vector2Int.right)
+            return Vector2Int.down;
+        else if (dir == Vector2Int.down)
+            return Vector2Int.left;
+        else if (dir == Vector2Int.left)
+            return Vector2Int.up;
+        else
+        {
+            Debug.LogError("ERROR");
+            return dir; // ERR
+        }
+    }
+
+    public static Vector2Int TurnLeft(Vector2Int dir)
+    {
+        if (dir == Vector2Int.up)
+            return Vector2Int.left;
+        else if (dir == Vector2Int.left)
+            return Vector2Int.down;
+        else if (dir == Vector2Int.down)
+            return Vector2Int.right;
+        else if (dir == Vector2Int.right)
+            return Vector2Int.up;
+        else
+        {
+            Debug.LogError("ERROR");
+            return dir; // ERR
+        }
+    }
+}
